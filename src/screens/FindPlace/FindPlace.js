ee4dff17340facc49ccfb26ef0a206f6f756e3a3
Hoai Thai <hthai@openwt.com>
2018-04-13 16:04:19 +0700

    animation

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { connect } from 'react-redux';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
@@ -8,7 +8,12 @@ class FindPlaceScreen extends Component {
   static navigatorStyle = {
     navBarButtonColor: "orange"
   }
-  
+
+  state = {
+    placesLoaded: false,
+    removeAnimation: new Animated.Value(1)
+  }
+
   constructor(props) {
     super(props);
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
@@ -22,6 +27,14 @@ class FindPlaceScreen extends Component {
         });
       }
     }
+  }
+
+  placesSearchHandler = () => {
+    Animated.timing(this.state.removeAnimation, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
   }
   
   itemSelectedHandler = key => {
@@ -37,13 +50,59 @@ class FindPlaceScreen extends Component {
     });
   }
   render () {
-    return (
-      <View>
+    let content = (
+      <Animated.View
+        style={{
+          opacity: this.state.removeAnimation,
+          transform:
+            [{
+              scale: this.state.removeAnimation.interpolate({
+                inputRange: [0, 1],
+                outputRange: [12, 1]
+              })
+            }]
+        }}>
+        <TouchableOpacity onPress={this.placesSearchHandler}>
+          <View style={styles.searchButton}>
+            <Text style={styles.searchButtonText}>Find places</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+    if (this.state.placesLoaded) {
+      content = (
         <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+      );
+    }
+    return (
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
       </View>
     );
   }   
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  listContainer: {
+
+  },
+  searchButton: {
+    borderColor: "orange",
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: "orange",
+    fontWeight: "bold",
+    fontSize: 26
+  }
+});
 
 const mapStateToProps = state => {
   return {
